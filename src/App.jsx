a53dcb5cb6ab5a03f639cbf99d452d4098a34521
CriinWa <nguyenkhoi.watanabe@gmail.com>
2025-09-21 21:46:59 +0700

    Upload btc REACT buổi 1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import BaiTapThucHanhLayout from './BaiTapLayoutComponent/BaiTapThucHanhLayout'
+
+export default function App() {
+  return (
+    <div>
+      <BaiTapThucHanhLayout />
+    </div>
+  )
+}
